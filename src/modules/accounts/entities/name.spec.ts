221e56d452473beacb40ae9f5994987420925ece
lmorais <lmorais.contato@gmail.com>
2023-08-15 23:20:24 -0300

    test: domain name entity

---
+++ src/modules/accounts/entities/name.spec.ts
@@ -0,0 +1,21 @@
+import Name from './name';
+
+describe('Name Entity', () => {
+  it('should be able to create a name with valid name', () => {
+    const name = Name.create('john') as Name;
+
+    expect(name.value).toBe('john');
+  });
+
+  it('should not be able to create a name with invalid name (when the same a not send)', () => {
+    expect(() => Name.create('')).toThrow();
+  });
+
+  it('should not be able to create a name with invalid name (when the same less 3 chars)', () => {
+    expect(() => Name.create('l')).toThrow();
+  });
+
+  it('should not be able to create a name with invalid name (when the same than 255 chars)', () => {
+    expect(() => Name.create('l'.repeat(256))).toThrow();
+  });
+});
