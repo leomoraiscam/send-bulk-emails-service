1ca1f7ea1364297f062762a8414d5259a25e54d6
lmorais <lmorais.contato@gmail.com>
2023-08-17 16:24:44 -0300

    test: invalidPasswordLengthError when the password is incorrect

--- src/modules/accounts/useCases/RegisterUser/RegisterUser.spec.ts
+++ src/modules/accounts/useCases/RegisterUser/RegisterUser.spec.ts
@@ -1,4 +1,4 @@
-import RegisterUser from './RegisterUser';
+import RegisterUser, { RegisterUserResponse } from './RegisterUser';
 
 describe('Register User Use Case', () => {
   it('should be able to register new user', async () => {
@@ -10,17 +10,28 @@ describe('Register User Use Case', () => {
       password: 'test@1234',
     };
 
-    const user = await registerUser.execute({
+    const user = (await registerUser.execute({
       ...userData,
-    });
+    })) as RegisterUserResponse;
 
     expect(user.name).toBe('John Doe');
   });
 
-  // it.todo(
-  //   'should not be able to register new user with invalid data',
-  //   () => {}
-  // );
+  it('should not be able to register new user with invalid data', async () => {
+    const registerUser = new RegisterUser();
+
+    const userData = {
+      name: 'John Doe',
+      email: 'john@email.com',
+      password: 't1',
+    };
+
+    const response = await registerUser.execute({
+      ...userData,
+    });
+
+    expect(response).toEqual('InvalidPasswordLengthError');
+  });
 
   // it.todo(
   //   'should not be able to register new user with existing email',
