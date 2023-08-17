bb1431f1863fd2d99ce1d47f5d0ce53bd1c0b02a
lmorais <lmorais.contato@gmail.com>
2023-08-16 22:52:48 -0300

    feat: add get methods in user domain entity

--- src/modules/accounts/entities/user.ts
+++ src/modules/accounts/entities/user.ts
@@ -9,14 +9,26 @@ interface IUserProps {
 }
 
 class User {
-  public readonly name: Name;
-  public readonly email: Email;
-  public readonly password: Password;
+  private readonly _name: Name;
+  private readonly _email: Email;
+  private readonly _password: Password;
+
+  get name(): Name {
+    return this._name;
+  }
+
+  get email(): Email {
+    return this._email;
+  }
+
+  get password(): Password {
+    return this._password;
+  }
 
   constructor(name: Name, email: Email, password: Password) {
-    this.name = name;
-    this.email = email;
-    this.password = password;
+    this._name = name;
+    this._email = email;
+    this._password = password;
   }
 
   static create({ name, email, password }: IUserProps): User | Error {
