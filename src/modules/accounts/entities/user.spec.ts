9f47226489789b0b402ec73661caee3486452383
lmorais <lmorais.contato@gmail.com>
2023-08-16 22:47:41 -0300

    test: domain user entity

---
+++ src/modules/accounts/entities/user.spec.ts
@@ -0,0 +1,16 @@
+import Email from './email';
+import Name from './name';
+import Password from './password';
+import User from './user';
+
+describe('User Entity', () => {
+  it('should be able to create a user', () => {
+    const userOrError = User.create({
+      name: Name.create('John Doe') as Name,
+      email: Email.create('john@doe.com') as Email,
+      password: Password.create('123456') as Password,
+    }) as User;
+
+    expect(userOrError.name.value).toBe('John Doe');
+  });
+});
