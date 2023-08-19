216044cb432b206ee80ada9d85f60698407a2c8a
lmorais <lmorais.contato@gmail.com>
2023-08-19 11:33:24 -0300

    feat: implementation search sender use case

--- src/modules/senders/repositories/ISendersRepository.ts
+++ src/modules/senders/repositories/ISendersRepository.ts
@@ -1,9 +1,21 @@
 import Sender from '@modules/senders/entities/sender';
 
+export type SendersSearchParams = {
+  query?: string;
+  page: number;
+  perPage: number;
+};
+
+export type SendersSearchResult = {
+  data: Sender[];
+  totalCount: number;
+};
+
 interface ISendersRepository {
   findAll(): Promise<Sender[]>;
   findById(id: string): Promise<Sender>;
   findDefaultSender(): Promise<Sender>;
+  search(params: SendersSearchParams): Promise<SendersSearchResult>;
   create(sender: Sender): Promise<void>;
   save(sender: Sender): Promise<void>;
 }
---
+++ src/modules/senders/usecases/SearchSenders.ts
@@ -0,0 +1,33 @@
+import Sender from '@modules/senders/entities/sender';
+import ISendersRepository from '@modules/senders/repositories/ISendersRepository';
+
+type SearchSendersRequest = {
+  query?: string;
+  page?: number;
+  perPage?: number;
+};
+
+type SearchSendersResponse = {
+  data: Sender[];
+  totalCount: number;
+};
+
+export class SearchSenders {
+  constructor(private sendersRepository: ISendersRepository) {}
+
+  async execute({
+    query,
+    page = 1,
+    perPage = 20,
+  }: SearchSendersRequest): Promise<SearchSendersResponse> {
+    const { data, totalCount } = await this.sendersRepository.search({
+      query,
+      page,
+      perPage,
+    });
+
+    return { data, totalCount };
+  }
+}
+
+export default SearchSenders;
