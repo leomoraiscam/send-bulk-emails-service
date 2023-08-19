0666e39535b2d2c11dea00ac71d0c9a3895de33e
lmorais <lmorais.contato@gmail.com>
2023-08-18 22:34:38 -0300

    feat: add InvalidNameError and InvalidEmailError to sender module

---
+++ src/modules/senders/entities/errors/InvalidNameError.ts
@@ -0,0 +1,8 @@
+class InvalidNameError extends Error {
+  constructor(name: string) {
+    super(`The name "${name}" is invalid.`);
+    this.name = 'InvalidNameError';
+  }
+}
+
+export default InvalidNameError;
