513732ea8409b9745e2608fb54361d55040fee58
lmorais <lmorais.contato@gmail.com>
2023-08-28 09:38:59 -0300

    test: add validations to register user web controller when 400 status returned

--- src/modules/accounts/useCases/RegisterUser/RegisterUserController.spec.ts
+++ src/modules/accounts/useCases/RegisterUser/RegisterUserController.spec.ts
@@ -30,4 +30,89 @@ describe('Register User (e2e)', () => {
 
     expect(response.status).toBe(201);
   });
+
+  it('should not be able to register new user', async () => {
+    const request: IRegisterUserRequest = {
+      name: 'John Doe',
+      email: 'johndoe@gmail.com',
+      password: 't',
+    };
+
+    const hashProvider = {
+      generateHash: jest.fn(),
+      compareHash: jest.fn(),
+    };
+
+    const inMemoryUsersRepository = new InMemoryUsersRepository();
+    const registerUserUseCase = new RegisterUser(
+      inMemoryUsersRepository,
+      hashProvider
+    );
+    const registerUserController = new RegisterUserController(
+      registerUserUseCase
+    );
+
+    const response = await registerUserController.handle(request);
+
+    expect(response.status).toBe(400);
+    expect(response.body.data).toBe('InvalidPasswordLengthError');
+  });
+
+  it('should not be able to register new user when is missing name', async () => {
+    const request: IRegisterUserRequest = {
+      name: '',
+      email: 'johndoe@gmail.com',
+      password: 'test@123',
+    };
+
+    const hashProvider = {
+      generateHash: jest.fn(),
+      compareHash: jest.fn(),
+    };
+
+    const inMemoryUsersRepository = new InMemoryUsersRepository();
+    const registerUserUseCase = new RegisterUser(
+      inMemoryUsersRepository,
+      hashProvider
+    );
+    const registerUserController = new RegisterUserController(
+      registerUserUseCase
+    );
+
+    const response = await registerUserController.handle(request);
+
+    expect(response.status).toBe(400);
+    expect(response.body.data).toBe(
+      '[MissingParamError: Missing parameter from request: name.]'
+    );
+  });
+
+  it('should not be able to register new user when is missing email', async () => {
+    const request: IRegisterUserRequest = {
+      name: 'John Doe',
+      email: '',
+      password: 'test@123',
+    };
+
+    const hashProvider = {
+      generateHash: jest.fn(),
+      compareHash: jest.fn(),
+    };
+
+    const inMemoryUsersRepository = new InMemoryUsersRepository();
+    const registerUserUseCase = new RegisterUser(
+      inMemoryUsersRepository,
+      hashProvider
+    );
+    const registerUserController = new RegisterUserController(
+      registerUserUseCase
+    );
+
+    const response = await registerUserController.handle(request);
+
+    expect(response.status).toBe(400);
+    expect(response.body.data).toBe(
+      '[MissingParamError: Missing parameter from request: email.]'
+    );
+  });
 });
