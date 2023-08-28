e4d6e4fed4ed88b2db56be3c4664a4c51895fc33
lmorais <lmorais.contato@gmail.com>
2023-08-28 09:38:43 -0300

    feat: add validations to register user web controller when 400 status returned

--- src/modules/accounts/useCases/RegisterUser/RegisterUserController.ts
+++ src/modules/accounts/useCases/RegisterUser/RegisterUserController.ts
@@ -1,11 +1,42 @@
+// eslint-disable-next-line max-classes-per-file
 import { IRegisterUserPayload } from './dtos/IRegisterUserPayload';
 import { RegisterUser } from './RegisterUser';
+
+export class MissingParamError extends Error {
+  public readonly name = 'MissingParamError';
+
+  constructor(param: string) {
+    super(`Missing parameter from request: ${param}.`);
+  }
+}
 
 export class RegisterUserController {
   constructor(private registerUser: RegisterUser) {}
 
   async handle(request: IRegisterUserPayload) {
     const response = await this.registerUser.execute(request);
+
+    if (!request.name || !request.email) {
+      let missing = !request.name ? 'name ' : '';
+
+      missing += !request.email ? 'email' : '';
+
+      return {
+        status: 400,
+        body: {
+          data: new MissingParamError(missing.trim()),
+        },
+      };
+    }
+
+    if (response instanceof Error) {
+      return {
+        status: 400,
+        body: {
+          data: response.name,
+        },
+      };
+    }
 
     return {
       status: 201,
