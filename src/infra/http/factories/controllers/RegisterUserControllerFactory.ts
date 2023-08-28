82778ecf7abdee0d9df24e8dfdcb615cb5965db0
lmorais <lmorais.contato@gmail.com>
2023-08-28 18:08:30 -0300

    feat: add accounts factory infra

---
+++ src/infra/http/factories/controllers/RegisterUserControllerFactory.ts
@@ -0,0 +1,19 @@
+import { InMemoryHashProvider } from 'infra/providers/HashProvider/in-memory/InMemoryHashProvider';
+
+import { InMemoryUsersRepository } from '@modules/accounts/repositories/in-memory/InMemoryUsersRepository';
+import { RegisterUser } from '@modules/accounts/useCases/RegisterUser/RegisterUser';
+import { RegisterUserController } from '@modules/accounts/useCases/RegisterUser/RegisterUserController';
+
+export const makeRegisterUserController = (): RegisterUserController => {
+  const inMemoryHashProvider = new InMemoryHashProvider();
+  const inMemoryUsersRepository = new InMemoryUsersRepository();
+
+  const registerUser = new RegisterUser(
+    inMemoryUsersRepository,
+    inMemoryHashProvider
+  );
+
+  const registerUserController = new RegisterUserController(registerUser);
+
+  return registerUserController;
+};
