15432527d800675bfa662824c254b5b3c46b4b48
lmorais <lmorais.contato@gmail.com>
2023-09-02 11:21:28 -0300

    refactor: use either in user domain entity

--- src/modules/accounts/entities/user.spec.ts
+++ src/modules/accounts/entities/user.spec.ts
@@ -2,12 +2,16 @@ import { Email, Name, Password, User } from '.';
 
 describe('User Domain Entity', () => {
   it('should be able to create a user', () => {
-    const userOrError = User.create({
-      name: Name.create('John Doe') as Name,
-      email: Email.create('john@doe.com') as Email,
-      password: Password.create('123456') as Password,
-    }) as User;
+    const nameOrError = Name.create('John Doe').value as Name;
+    const emailOrError = Email.create('john@doe.com').value as Email;
+    const passwordOrError = Password.create('123456', true).value as Password;
 
-    expect(userOrError.name.value).toBe('John Doe');
+    const userOrError = User.create({
+      name: nameOrError,
+      email: emailOrError,
+      password: passwordOrError,
+    });
+
+    expect(userOrError.isRight()).toBeTruthy();
   });
 });
--- src/modules/accounts/entities/user.ts
+++ src/modules/accounts/entities/user.ts
@@ -1,30 +1,48 @@
+import { Either, right } from '@core/logic/Either';
+import { InvalidNameError } from '@modules/senders/entities/errors';
+
 import { Name, Email, Password } from '.';
 import { IUserProps } from './dtos/IUserProps';
+import {
+  InvalidEmailError,
+  InvalidPasswordLengthError,
+  InvalidSecurityPasswordError,
+} from './errors';
 
 export class User {
   private readonly _name: Name;
   private readonly _email: Email;
   private readonly _password: Password;
 
-  get name(): Name {
-    return this._name;
-  }
-
-  get email(): Email {
-    return this._email;
-  }
-
-  get password(): Password {
-    return this._password;
-  }
-
-  constructor(name: Name, email: Email, password: Password) {
+  private constructor(name: Name, email: Email, password: Password) {
     this._name = name;
     this._email = email;
     this._password = password;
   }
 
-  static create({ name, email, password }: IUserProps): User | Error {
-    return new User(name, email, password);
+  public get name(): Name {
+    return this._name;
+  }
+
+  public get email(): Email {
+    return this._email;
+  }
+
+  public get password(): Password {
+    return this._password;
+  }
+
+  public static create({
+    name,
+    email,
+    password,
+  }: IUserProps): Either<
+    | InvalidEmailError
+    | InvalidNameError
+    | InvalidPasswordLengthError
+    | InvalidSecurityPasswordError,
+    User
+  > {
+    return right(new User(name, email, password));
   }
 }
