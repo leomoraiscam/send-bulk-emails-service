8ab5a3b2cc10d102ffca05e69246769adffce5dc
lmorais <lmorais.contato@gmail.com>
2023-09-04 16:39:36 -0300

    feat: add singleton pattern in fake object memory users repository

--- src/modules/accounts/repositories/in-memory/InMemoryUsersRepository.ts
+++ src/modules/accounts/repositories/in-memory/InMemoryUsersRepository.ts
@@ -1,9 +1,20 @@
+/* eslint-disable no-class-assign */
 import { User } from '@modules/accounts/entities';
 
 import { IUsersRepository } from '../IUsersRepository';
 
 export class InMemoryUsersRepository implements IUsersRepository {
   private repository: User[] = [];
+
+  private static INSTANCE: InMemoryUsersRepository;
+
+  public static getInstance(): InMemoryUsersRepository {
+    if (!InMemoryUsersRepository.INSTANCE) {
+      InMemoryUsersRepository.INSTANCE = new InMemoryUsersRepository();
+    }
+
+    return InMemoryUsersRepository.INSTANCE;
+  }
 
   async findByEmail(email: string): Promise<User | null> {
     const user = this.repository.find((user) => user.email.value === email);
